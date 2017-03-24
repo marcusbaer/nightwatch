fbbae6fe0d6464e36b0632df1e43ce156efe3b9a
Marcus Baer <marcus.baer@publicispixelpark.de>
2017-03-24 15:56:42 +0100

    force a failure

--- test/specs/failure.js
+++ test/specs/failure.js
@@ -9,12 +9,12 @@ module.exports = {
     browser
       .pause(1000)
       .setValue('#input', 'nightwatch')
-      .expect.element('#result').text.to.equal('test')
+      .expect.element('#result').text.to.equal('foodwatch')
     browser
       .pause(1000)
       .clearValue('#input')
       .setValue('#input', 'test')
-      .expect.element('#result').text.to.equal('nightwatch')
+      .expect.element('#result').text.to.equal('test')
     browser
       .pause(1000)
       .end()
